42cb49d5c54f86322bdb685d25af78450d514529
Zrewlate <zeroualihamza110@gmail.com>
2026-08-31 21:12:39 -0400

    Nettoyer le point de demarrage

--- src/gestionstock/App.java
+++ src/gestionstock/App.java
@@ -1,35 +1,7 @@
 package gestionstock;
 
-import gestionstock.dao.StockDao;
-import gestionstock.model.StockItem;
-
-import java.time.LocalDate;
-
 public class App {
     public static void main(String[] args) {
-
-        StockDao dao = new StockDao();
-
-        StockItem item = new StockItem(
-                0,
-                1002,
-                LocalDate.of(2026, 8, 30),
-                "Rabat",
-                "Adidas Samba",
-                41,
-                800,
-                450,
-                40,
-                310,
-                LocalDate.of(2026, 9, 3),
-                "En cours"
-        );
-
-        try {
-            dao.insert(item);
-            System.out.println("Commande ajoutee avec succes !");
-        } catch (Exception e) {
-            System.out.println("Erreur : " + e.getMessage());
-        }
+        System.out.println("Gestion de stock demarre !");
     }
 }
