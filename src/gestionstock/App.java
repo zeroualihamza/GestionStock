1734d8f793630be353ed5bdaf677ded6c9ca6a21
Zrewlate <zeroualihamza110@gmail.com>
2026-08-31 21:54:28 -0400

    Nettoyer App apres test suppression

--- src/gestionstock/App.java
+++ src/gestionstock/App.java
@@ -1,17 +1,7 @@
 package gestionstock;
 
-import gestionstock.dao.StockDao;
-
 public class App {
     public static void main(String[] args) {
-
-        StockDao dao = new StockDao();
-
-        try {
-            dao.delete(2);
-            System.out.println("Commande supprimee avec succes !");
-        } catch (Exception e) {
-            System.out.println("Erreur : " + e.getMessage());
-        }
+        System.out.println("Gestion de stock demarre !");
     }
 }
