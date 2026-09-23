6a271b569394d24dabeaebfed52db709c132a2e5
Zrewlate <zeroualihamza110@gmail.com>
2026-09-23 19:07:48 -0400

    Nettoyer App apres test fournisseur

--- src/gestionstock/App.java
+++ src/gestionstock/App.java
@@ -1,30 +1,10 @@
 package gestionstock;
 
-import gestionstock.dao.SupplierDao;
-import gestionstock.model.SupplierOperation;
-
-import java.time.LocalDate;
+import gestionstock.ui.LoginFrame;
 
 public class App {
     public static void main(String[] args) {
-
-        SupplierDao dao = new SupplierDao();
-
-        SupplierOperation operation = new SupplierOperation(
-                0,
-                LocalDate.of(2026, 9, 23),
-                "Achat fournisseur test",
-                "Nike Air Max",
-                500,
-                200,
-                300
-        );
-
-        try {
-            dao.insert(operation);
-            System.out.println("Operation fournisseur ajoutee avec succes !");
-        } catch (Exception e) {
-            System.out.println("Erreur : " + e.getMessage());
-        }
+        LoginFrame frame = new LoginFrame();
+        frame.setVisible(true);
     }
 }
